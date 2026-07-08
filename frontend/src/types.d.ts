a59cd04981f0268ac0b0d1c98485f9db60ad227f
h1rshh <harshvardhanshekhawat815@gmail.com>
2026-07-08 22:28:40 +0530

    Fix TypeScript JSX IntrinsicElements definition for iconify-icon custom element

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,7 +1,23 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "iconify-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      icon: string;
-    };
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "iconify-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        icon: string;
+        class?: string;
+      };
+    }
+  }
+}
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      "iconify-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        icon: string;
+        class?: string;
+      };
+    }
   }
 }
